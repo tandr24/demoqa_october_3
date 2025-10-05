e21ed31b2ee9e6ed4b0aabe806f365df459ebb58
Tatiana Andreeva <andretat8@gmail.com>
2025-10-05 12:35:38 +0200

    applied comments from Alex

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,27 +1,19 @@
-
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.commands.SelectRadio;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class SimpleTest {
-
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+      //  Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
@@ -33,9 +25,8 @@ public class SimpleTest {
         $("#userEmail").setValue("email@mail.com");
 
         //Select gender
-       $("#genterWrapper div.col-md-9.col-sm-12 #gender-radio-2").doubleClick();
-        //$("#gender-radio-2").selectRadio("Female");
-        //       $(By.name("gender")).selectRadio("Female").click();
+        // $("#genterWrapper div.col-md-9.col-sm-12 #gender-radio-2").doubleClick();
+        $("#genterWrapper").$(byText("Female")).click();
 
         $("#userNumber").setValue("0123456789");
 
@@ -48,17 +39,17 @@ public class SimpleTest {
         $(".react-datepicker__day.react-datepicker__day--019").click();
 
         //Select Subject
-        $("#subjectsContainer").click();
         $("#subjectsInput").setValue("Ma").pressEnter();
-        $("#subjectsContainer").click();
         $("#subjectsInput").setValue("Ch").pressEnter();
 
         //Select Hobbies
-        $("#hobbiesWrapper div.col-md-9.col-sm-12 [for=hobbies-checkbox-3]").click();
-        $("#hobbiesWrapper div.col-md-9.col-sm-12 [for=hobbies-checkbox-2]").click();
+//        $("#hobbiesWrapper div.col-md-9.col-sm-12 [for=hobbies-checkbox-3]").click();
+//        $("#hobbiesWrapper div.col-md-9.col-sm-12 [for=hobbies-checkbox-2]").click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
 
         //File
-        $("#uploadPicture").uploadFile(new File("/Users/tandre/Downloads/img.png"));
+        $("#uploadPicture").uploadFromClasspath("img.png");
 
         //Address
         $("#currentAddress").setValue("Address");
@@ -72,6 +63,17 @@ public class SimpleTest {
         //Submit
         $("#submit").click();
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("div.table-responsive").shouldHave(text("Name Surname"));
+        $("div.table-responsive").shouldHave(text("email@mail.com"));
+        $("div.table-responsive").shouldHave(text("Female"));
+        $("div.table-responsive").shouldHave(text("0123456789"));
+        $("div.table-responsive").shouldHave(text("19 May,1998"));
+        $("div.table-responsive").shouldHave(text("Maths, Chemistry"));
+        $("div.table-responsive").shouldHave(text("Music, Reading"));
+        $("div.table-responsive").shouldHave(text("img.png"));
+        $("div.table-responsive").shouldHave(text("Address"));
+        $("div.table-responsive").shouldHave(text("NCR Delhi"));
+
 
     }
 
